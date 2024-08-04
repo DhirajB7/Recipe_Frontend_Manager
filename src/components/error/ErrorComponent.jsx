d7e5455d7133470d248ccfc063309c0931df1b4a
Dhiraj <dhiraj.basavaraju@gmail.com>
2024-08-05 02:53:54 +0530

    error page done

---
+++ src/components/error/ErrorComponent.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import "./ErrorComponent.css";
+import { Grid, Typography } from "@mui/material";
+import { useNavigate } from "react-router-dom";
+import notFound from "../../static/wreck-it-ralph-ralph.gif";
+import ButtonCommon from "../common/button/ButtonCommon";
+
+const ErrorComponent = ({ errorData }) => {
+  const navigate = useNavigate();
+
+  return (
+    <Grid
+      container
+      flexDirection="column"
+      justifyContent="space-evenly"
+      alignItems="center"
+      className="container"
+    >
+      <Grid item>
+        <Typography variant="h2" className="heading">
+          {errorData.title}
+        </Typography>
+      </Grid>
+
+      <Grid item>
+        <img src={notFound} alt="404" />
+      </Grid>
+
+      <Grid item>
+        <Typography variant="h4" className="heading">
+          {errorData.message}
+        </Typography>
+      </Grid>
+
+      <Grid item>
+        <ButtonCommon
+          text={errorData.btnText}
+          onClick={() => navigate(errorData.homePageLink)}
+        />
+      </Grid>
+    </Grid>
+  );
+};
+
+export default ErrorComponent;
--- src/pages/error/Error.jsx
+++ src/pages/error/Error.jsx
@@ -1,12 +1,18 @@
-import { Typography } from "@mui/material";
 import { useEffect } from "react";
 import { updateTitle } from "../../utils/Common";
+import ErrorComponent from "../../components/error/ErrorComponent";
+import { Box } from "@mui/material";
+import words from "../../utils/words.json";
 
 const Error = () => {
   useEffect(() => {
     updateTitle();
   }, []);
-  return <Typography variant="h3">Error</Typography>;
+  return (
+    <Box className={"container"}>
+      <ErrorComponent heading={words.error.title} errorData={words.error} />
+    </Box>
+  );
 };
 
 export default Error;
